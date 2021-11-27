1929261548791dc5c0b6689427495201ff82ec0e
Felix Häcker <haeckerfelix@gnome.org>
2021-11-27 20:05:33 +0100

    session_stats: Make fields public

--- src/session_stats.rs
+++ src/session_stats.rs
@@ -3,25 +3,25 @@ use crate::rpc::RpcResponseArguments;
 #[derive(Debug, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct SessionStats {
-    active_torrent_count: i64,
+    pub active_torrent_count: i32,
     #[serde(rename = "cumulative-stats")]
-    cumulative_stats: StatsDetails,
+    pub cumulative_stats: StatsDetails,
     #[serde(rename = "current-stats")]
-    current_stats: StatsDetails,
-    download_speed: i64,
-    paused_torrent_count: i64,
-    torrent_count: i64,
-    upload_speed: i64,
+    pub current_stats: StatsDetails,
+    pub download_speed: i32,
+    pub paused_torrent_count: i32,
+    pub torrent_count: i32,
+    pub upload_speed: i32,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct StatsDetails {
-    downloaded_bytes: i64,
-    files_added: i64,
-    seconds_active: i64,
-    session_count: i64,
-    uploaded_bytes: i64,
+    pub downloaded_bytes: i64,
+    pub files_added: i64,
+    pub seconds_active: i64,
+    pub session_count: i64,
+    pub uploaded_bytes: i64,
 }
 
 impl RpcResponseArguments for SessionStats {}
